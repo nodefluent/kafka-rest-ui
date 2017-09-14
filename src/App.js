447ae1ea65c84db993307bb67510d867e04c347f
yacut <roman.rogozhnikov@gmail.com>
2017-09-14 09:11:10 +0200

    add another json view fix css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import ReactJson from 'react-json-view';
+import JSONTree from 'react-json-tree';
 import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
 import Icon from 'react-icons-kit';
 import { ic_list } from 'react-icons-kit/md/ic_list';
@@ -24,6 +25,27 @@ type Props = {
   mounted: void,
   topics: Topics,
   consumers: Consumers,
+};
+
+
+const colorTheme = {
+  scheme: 'google',
+  base00: '#1d1f21',
+  base01: '#282a2e',
+  base02: '#373b41',
+  base03: '#969896',
+  base04: '#b4b7b4',
+  base05: '#c5c8c6',
+  base06: '#e0e0e0',
+  base07: '#ffffff',
+  base08: '#CC342B',
+  base09: '#F96A38',
+  base0A: '#FBA922',
+  base0B: '#198844',
+  base0C: '#3971ED',
+  base0D: '#3971ED',
+  base0E: '#A36AC7',
+  base0F: '#3971ED',
 };
 
 class App extends Component<Props> {
@@ -82,37 +104,53 @@ class App extends Component<Props> {
               }
             </SideNav>
           </div>
-          <div><Tabs>
+          <Tabs style={{ height: '-webkit-calc(100% - 42px)' }}>
             <TabList>
               <Tab>Messages</Tab>
+              <Tab>Messages (another view)</Tab>
               <Tab>Partitions</Tab>
               <Tab>Configs</Tab>
             </TabList>
 
-            <TabPanel>
+            <TabPanel style={{ height: '100%' }}>
               {this.props.consumers.loading && <div className="Progress">
                 <Icon className="load" icon={ic_loop} />
                 {this.props.consumers.progress}
               </div> }
               {!this.props.consumers.loading && this.props.consumers.records.length > 0 &&
-              <div style={{ display: 'flex' }}>
+              <div className="Messages">
                 <ReactJson
                   src={this.props.consumers.records}
                   name={null}
                   displayDataTypes={false}
+                  iconStyle={'circle'}
                 />
-              </div>
-              }
+              </div>}
+              {!this.props.consumers.loading && this.props.consumers.records.length === 0 && (<div className="NoContent">No records</div>)}
+            </TabPanel>
+            <TabPanel style={{ height: '100%' }}>
+              {this.props.consumers.loading && <div className="Progress">
+                <Icon className="load" icon={ic_loop} />
+                {this.props.consumers.progress}
+              </div> }
+              {!this.props.consumers.loading && this.props.consumers.records.length > 0 &&
+              <div className="Messages">
+                <JSONTree
+                  data={this.props.consumers.records}
+                  theme={colorTheme}
+                  hideRoot={false}
+                  shouldExpandNode={() => true}
+                />
+              </div>}
               {!this.props.consumers.loading && this.props.consumers.records.length === 0 && (<div className="NoContent">No records</div>)}
             </TabPanel>
             <TabPanel>
-              <h2>Partitions</h2>
+              <h2>Partiotions</h2>
             </TabPanel>
             <TabPanel>
               <h2>Configs</h2>
             </TabPanel>
           </Tabs>
-          </div>
         </div>
       </div>
     );

c14ca265496d5457bd3f1b13c61921c7c971ee74
yacut <roman.rogozhnikov@gmail.com>
2017-09-15 13:04:05 +0200

    remove caddy add serve

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,9 @@
 
 export REACT_APP_KAFKA_REST_URL="${REACT_APP_KAFKA_REST_URL:-http://localhost:8082}"
 export REACT_APP_TIMEOUT="${REACT_APP_TIMEOUT:-2000}"
-export PROXY_SKIP_VERIFY="${PROXY_SKIP_VERIFY:-false}"
-export PROXY_URL="${PROXY_URL}"
+export REACT_APP_PROXY=${PROXY}
+export PROXY=${PROXY}
+export PROXY_PARAMS=""
 export HTTP_PORT=${HTTP_PORT:-8000}
 export BASIC_AUTH_USER=${BASIC_AUTH_USER}
 export BASIC_AUTH_PASSWORD=${BASIC_AUTH_PASSWORD}
@@ -12,30 +13,21 @@ if [[ -z "$REACT_APP_KAFKA_REST_URL" ]]; then
     echo "Kafka REST URL was not set via REACT_APP_KAFKA_REST_URL environment variable."
 fi
 
-if [ ! -z "$BASIC_AUTH_USER" ]; then
-  echo 'basicauth {$REACT_APP_KAFKA_REST_URL} {$BASIC_AUTH_USER} {$BASIC_AUTH_PASSWORD}' >>/caddy/Caddyfile
-fi
-
-if echo "$PROXY" | egrep -sq "true|TRUE|y|Y|yes|YES|1"; then
-  PROXY_URL=${REACT_APP_KAFKA_REST_URL}
-  REACT_APP_KAFKA_REST_URL="/api/kafka-rest"
-  if echo "$PROXY_SKIP_VERIFY" | egrep -sq "true|TRUE|y|Y|yes|YES|1"; then
-    echo 'proxy /api/kafka-rest {$PROXY_URL} {
-  without /api/kafka-rest
-  insecure_skip_verify
-}' >>/caddy/Caddyfile
-  else
-    echo 'proxy /api/kafka-rest {$PROXY_URL} {
-  without /api/kafka-rest
-}' >>/caddy/Caddyfile
-  fi
-fi
-
 echo "Building application..."
 NODE_ENV=production yarn build
 
 echo
 echo "Starting server..."
 echo "http://0.0.0.0:${HTTP_PORT}"
-cat /caddy/Caddyfile
-exec /caddy/caddy -conf /caddy/Caddyfile -quiet
+
+if echo "$PROXY" | egrep -sq "true|TRUE|y|Y|yes|YES|1"; then
+  PROXY_PARAMS="--cors"
+fi
+
+if [ ! -z "$BASIC_AUTH_USER" ]; then
+  export SERVE_USER=${BASIC_AUTH_USER}
+  export SERVE_PASSWORD=${BASIC_AUTH_PASSWORD}
+  exec serve --auth ${PROXY_PARAMS} --port ${HTTP_PORT} --single build
+else
+  exec serve ${PROXY_PARAMS} --port ${HTTP_PORT} --single build
+fi

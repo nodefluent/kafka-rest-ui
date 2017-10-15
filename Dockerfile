7295bc400b013572a5423aeda2af9a3e0cd7e80a
yacut <roman.rogozhnikov@gmail.com>
2017-10-15 23:03:56 +0200

    add nginx

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:alpine
+MAINTAINER nodefluent
+
+ENV KAFKA_REST_UI_VERSION=0.6.1
+ENV NODE_ENV=production
+COPY . /usr/share/nginx/kafka-rest-ui/
+COPY ./nginx/nginx.conf /etc/nginx/nginx.conf
+COPY ./nginx/kafka-rest-ui.template /etc/nginx/conf.d/kafka-rest-ui.template
+COPY ./nginx/kafka-rest-ui-proxy-location.template /etc/nginx/conf.d/kafka-rest-ui-proxy-location.template
+WORKDIR /usr/share/nginx/kafka-rest-ui/
+
+RUN apk update \
+	&& apk add --no-cache libcap bash curl git nginx gettext apache2-utils \
+	&& ln -sf /dev/stdout /var/log/nginx/access.log \
+    && ln -sf /dev/stderr /var/log/nginx/error.log \
+    && yarn install
+
+STOPSIGNAL SIGTERM
+EXPOSE ${HTTP_PORT:-8000}
+CMD ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "${DEBUG}" ]; then
+  set +o xtrace
+else
+  set -o xtrace
+fi
+
+export REACT_APP_KAFKA_REST_URL="${REACT_APP_KAFKA_REST_URL:-http://localhost:8082}"
+export REACT_APP_TIMEOUT="${REACT_APP_TIMEOUT:-2000}"
+export REACT_APP_PROXY="${PROXY}"
+export PROXY_URL="${PROXY_URL}"
+export REACT_APP_DEBUG="${DEBUG}"
+export HTTP_PORT=${HTTP_PORT:-8000}
+export BASIC_AUTH_USER=${BASIC_AUTH_USER}
+export BASIC_AUTH_PASSWORD=${BASIC_AUTH_PASSWORD}
+export BASIC_AUTH=""
+
+if [[ -z "$REACT_APP_KAFKA_REST_URL" ]]; then
+    echo "Kafka REST URL was not set via REACT_APP_KAFKA_REST_URL environment variable."
+fi
+
+if echo "$PROXY" | grep -sqE "true|TRUE|y|Y|yes|YES|1"; then
+  export PROXY_URL=${REACT_APP_KAFKA_REST_URL}
+  export REACT_APP_KAFKA_REST_URL="/api/kafka-rest"
+  envsubst '$REACT_APP_KAFKA_REST_URL $PROXY_URL' < /etc/nginx/conf.d/kafka-rest-ui-proxy-location.template > /etc/nginx/conf.d/kafka-rest-ui-proxy.location
+  cat /etc/nginx/conf.d/kafka-rest-ui-proxy.location
+fi
+
+if [ ! -z "${BASIC_AUTH_USER}" ]; then
+	htpasswd -cb /etc/nginx/.htpasswd "${BASIC_AUTH_USER}" "${BASIC_AUTH_PASSWORD}"
+	export BASIC_AUTH='auth_basic "Restricted";
+      auth_basic_user_file /etc/nginx/.htpasswd;'
+fi
+
+envsubst '$HTTP_PORT $BASIC_AUTH' < /etc/nginx/conf.d/kafka-rest-ui.template > /etc/nginx/conf.d/kafka-rest-ui.conf
+cat /etc/nginx/conf.d/kafka-rest-ui.conf
+
+echo "Building application..."
+yarn build
+
+echo
+echo "Starting server..."
+echo "http://0.0.0.0:${HTTP_PORT}"
+exec nginx -g "daemon off;"
